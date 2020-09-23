913fcf3c01aed6c38ab108c3e993f2e46a00687f
Dmitry <mmmmm@mmmmm.mmmm>
2020-09-23 23:13:21 +0500

    56

--- init.sh
+++ init.sh
@@ -1,5 +1,3 @@
 sudo rm /etc/nginx/sites-enabled/default
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
 sudo /etc/init.d/nginx restart
-cd web
-sudo gunicorn -b 0.0.0.0:8080 hello:app
