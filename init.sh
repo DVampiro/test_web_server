6a445c3a765c03664146fc0d536ce548828ae807
Dmitry <mmmmm@mmmmm.mmmm>
2020-09-23 20:26:58 +0500

    -

--- init.sh
+++ init.sh
@@ -1,5 +1,5 @@
 sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
-sudo ln -sf /home/box/web/etc/hello.py /etc/gunicorn.d/test.py
+sudo ln -sf /home/box/web/hello.py /etc/gunicorn.d/test.py
 sudo rm -rf /etc/nginx/sites-enabled/default
 sudo /etc/init.d/nginx restart
 sudo gunicorn -b 0.0.0.0:8080 hello:app
